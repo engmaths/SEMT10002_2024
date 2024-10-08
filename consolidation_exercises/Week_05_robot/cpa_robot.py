6adc0d6576bc1344fb39ad93caaa86f8839b131e
aeagr-l <arthur.richards@bristol.ac.uk>
2024-10-08 10:35:14 +0100

    Tidying up robot library

--- consolidation_exercises/Week_05_robot/cpa_robot.py
+++ consolidation_exercises/Week_05_robot/cpa_robot.py
@@ -1,37 +1,38 @@
 from matplotlib import pyplot as plt
+from math import cos, sin
+
+_x = 0.
+_y = 0.
+_theta = 0.
+
+history = [(_x,_y,_theta)]
+
+def reset_robot(x_init = 0.0, y_init=0.0, theta_init = 0.0):
+    global _x, _y, _theta
+    _x = x_init
+    _y = y_init
+    _theta = theta_init
+    history.clear()
+    history.append((_x,_y,_theta))
 
 WHEEL_SEP = 5.0
 
-x = 0.
-y = 0.
-theta = 0.
-
-history = [(x,y,theta)]
-
-def reset_robot():
-    global x, y, theta
-    x = 0
-    y = 0
-    history.clear()
-    history.append((x,y,theta))
-
 def drive(right_turn, left_turn):
-    global x, y, theta
+    global _x, _y, _theta
     turn_angle = (right_turn - left_turn)/WHEEL_SEP
     arc_length = 0.5*(right_turn + left_turn)
-    curvature = turn_angle/arc_length
-    x = x + right_turn
-    y = y + left_turn
-    history.append((x,y,theta))
+    _x = _x + right_turn
+    _y = _y + left_turn
+    history.append((_x,_y,_theta))
 
 def position_x():
-    return x
+    return _x
 
 def position_y():
-    return y
+    return _y
 
 def orientation():
-    return theta
+    return _theta
 
 def sensor_left():
     return True
