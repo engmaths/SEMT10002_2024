c3a57fbe49e507b7a2779a18a7a7c3c217dd5411
aeagr <aeagr@DESKTOP-GIUBSQQ>
2024-10-14 13:16:00 +0100

    Finished lab sheet and sample submission

---
+++ consolidation_exercises/Week_05_robot/robot_challenge.py
@@ -0,0 +1,7 @@
+from cpa_robot import position_x, position_y, orientation, drive, plot_path, reset_robot, plot_robot, sensor_left, sensor_middle, sensor_right
+reset_robot()
+
+# edit this comment - challenge is to ???
+'???'
+
+plot_path()
