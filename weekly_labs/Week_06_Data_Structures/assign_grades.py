601c85e31fedb3ab4b72152ff8399c90b9e6b0ec
Garrad <m.garrad@bristol.ac.uk>
2024-10-28 07:52:48 +0000

    Forgot to add some files..

---
+++ weekly_labs/Week_06_Data_Structures/assign_grades.py
@@ -0,0 +1,44 @@
+'''
+Student	Question 1	Question 2	Question 3
+Martin	6	1	4
+Arthur	3	8	4
+Hemma	7	4	5
+Josh	4	7	3
+'''
+
+import math 
+
+names = ["Martin", "Arthur", "Hemma", "Josh"]
+Q1 = [6, 3, 7, 4]
+Q2 = [1, 8, 4, 7]
+Q3 = [4, 4, 5, 3]
+
+#Write your code for calculating the total score for each student here.
+
+'???'
+
+#Write your code for calculating the mean score here
+
+'???'
+
+print('mean', mean)
+
+#Write your code for calculating the standard deviation here
+
+'???'
+
+print('standard_deviation', standard_deviation)
+
+#Write your code for calculating the normalised marks here
+
+'???'
+
+#Write your code for assigning grades here
+
+grades = dict()
+
+'???'
+
+for name in names:
+	print(name, grades[name])
+
